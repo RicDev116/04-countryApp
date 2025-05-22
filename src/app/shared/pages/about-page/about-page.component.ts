734638948b64a0d059f5d8ff2e081f5ec5c02c82
RicDev1167 <ricardo_29leyva@outlook.com>
2025-05-21 18:46:36 -0600

    Router Module yPáginas - SPA

---
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  template: `<p>about-page works!</p>`,
+  styleUrl: './about-page.component.css',
+  standalone: false,
+})
+export class AboutPageComponent { }
---
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  template: `<p>home-page works!</p>`,
+  styleUrl: './home-page.component.css',
+  standalone:false
+})
+export class HomePageComponent { }
